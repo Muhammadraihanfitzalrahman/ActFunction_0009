d20750d4aa2fdb9b417a063f40b9fc511307d6fe
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-03-03 15:47:32 +0700

    setup basic C++ program

---
+++ Praktikum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// Deklarasi Global
+float p, l;
+
+// Implementasi Prosedur & Fungsi
+void Input()
+{
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+
+float LuasPersegi()
+{
+    return p * l;
+}
+
+void Output()
+{
+    cout << "Hasilnya = " << LuasPersegi();
+}
+
+// Program Utama
+int main() 
+{ // start
+    Input();
+    Output();
+}//selesai
--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -1,31 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Deklarasi Global
-float p, l;
-
-// Implementasi Prosedur & Fungsi
-void Input()
-{
-    cout << "Masukkan Panjang : ";
-    cin >> p;
-    cout << "Masukkan Lebar : ";
-    cin >> l;
+int main() {
+    return 0;
 }
-
-float LuasPersegi()
-{
-    return p * l;
-}
-
-void Output()
-{
-    cout << "Hasilnya = " << LuasPersegi();
-}
-
-// Program Utama
-int main() 
-{ // start
-    Input();
-    Output();
-}//selesai
