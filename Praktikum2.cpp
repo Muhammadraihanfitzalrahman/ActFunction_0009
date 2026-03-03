9c7034806e1cb6335b7f0e6fdfb7eb1d37f007e0
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-03-03 15:32:11 +0700

    Implementasi prosedur & fungsi

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -3,3 +3,22 @@ using namespace std;
 
 // Deklarasi Global
 float p, l;
+
+// Implementasi Prosedur & Fungsi
+void Input()
+{
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+
+float LuasPersegi()
+{
+    return p * l;
+}
+
+void Output()
+{
+    cout << "Hasilnya = " << LuasPersegi();
+}
